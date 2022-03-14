f75b7daecaaedf60eab02a68ae5bbb65ba64d1ca
Michael Boerman <michaelboerman@hey.com>
2022-03-14 16:47:45 -0400

    initial commit w/ MVP

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # covid_percentiles
- A Shiny app displaying statistics about a user's experience.
+ A Shiny app displaying a percentile of US covid cases as of a certain date..
+ 
+ https://michaelboerman.shinyapps.io/covid_percentiles/
---
+++ app.R
@@ -0,0 +1,48 @@
+library(shiny)
+library(COVID19)
+library(tidyverse)
+
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("COVID: How did you fare?"),
+    
+    h5("If life is a competition and not getting covid before the masses is something upon which you wnat to pride yourself, then this page helps you feel good about how long you may have fended off the virus -- or learn that you weren't ahead (er, behind) the curve as you hoped."),
+    h5("This page does not store any date you input. Your information stays private."),
+    h5("The code lives here: "),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            dateInput("user_covid_date", "When did you get Covid?", value = "2022-01-01")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            textOutput("percent")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    all_covid_data <- COVID19::covid19("USA")
+    total_confirmed_count <- all_covid_data %>% pull(confirmed) %>% tail(1) 
+    
+    percentile <- reactive(
+        all_covid_data %>% 
+            select(date, confirmed) %>% 
+            mutate(pct_of_total = confirmed / total_confirmed_count) %>% 
+            filter(date == input$user_covid_date) %>% 
+            pull(pct_of_total)
+    )
+    
+    output$percent <- renderText(
+        paste0("You got covid after ", round(percentile(), 4)*100, "% of people got it in the United States to date.")
+    )
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
